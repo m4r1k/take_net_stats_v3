7eb123ea778efd0121f6e8ec416af1ba80689fa0
Federico Iezzi <fiezzi@redhat.com>
2018-09-24 17:20:07 +0200

    Fix wrong variable _TAPParent

--- take_net_stats_v3.sh
+++ take_net_stats_v3.sh
@@ -44,7 +44,7 @@ done
 # Pull VIF Parents from Contrail using hardcoded vif0/
 for (( i=0 ; i<"${#_VIFLIST[@]}" ; i++ ))
 do
-	_ParentsTAP=$(vif --list|grep "Parent:vif0/${_VIFLIST[$i]}"|wc -l)
+	_TAPParent=$(vif --list|grep "Parent:vif0/${_VIFLIST[$i]}"|wc -l)
 	if [[ "${_TAPParents}" != "0" ]]; then
 		for _PARENT in $(vif --list|grep "Parent:vif0/${_VIFLIST[$i]}"|awk '{print $1}'|sed -e "s|vif0/||g")
 		do
